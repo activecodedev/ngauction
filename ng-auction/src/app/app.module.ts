ff6354baf5a7368702c726a2cd1e19f2e62bd183
Rafał Pachnicki <rafal.pachnicki@itmagination.com>
2020-04-16 00:57:19 +0200

    Home Component with Material Design

--- ng-auction/src/app/app.module.ts
+++ ng-auction/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatButtonModule } from '@angular/material/button';
@@ -9,6 +9,8 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { HttpClientModule } from "@angular/common/http";
+import { ProductService } from './shared/services/product.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
    declarations: [
@@ -23,7 +25,7 @@ import { HttpClientModule } from "@angular/common/http";
       FlexLayoutModule,
       HttpClientModule
    ],
-   providers: [],
+   providers: [ProductService],
    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ng-auction/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { map } from "rxjs/operators";
+import { Observable } from 'rxjs';
+import { MediaObserver } from "@angular/flex-layout";
+import { Product, ProductService } from '../shared/services';
+
+@Component({
+   selector: 'nga-home',
+   templateUrl: './home.component.html',
+   styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+   readonly columns$: Observable<number>;
+   readonly products$: Observable<Product[]>
+   readonly breakpointsToColumnsNumber = new Map([
+      ['xs', 1],
+      ['sm', 2],
+      ['md', 3],
+      ['lg', 4],
+      ['xl', 5]
+   ])
+
+   constructor(private media: MediaObserver, private productService: ProductService) {
+      this.products$ = this.productService.getAll();
+      this.columns$ = this.media.media$.pipe(
+         map(mc => <number>this.breakpointsToColumnsNumber.get(mc.mqAlias))
+      );
+   }
+
+   ngOnInit(): void {
+   }
+
+}
