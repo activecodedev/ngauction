15b9964b485556ce061f0ec896d11fac77c2f418
Rafał Pachnicki <rafal.pachnicki@hotmail.com>
2020-04-18 20:53:06 +0200

    Add search module

--- ng-auction/src/app/app.module.ts
+++ ng-auction/src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { HttpClientModule } from "@angular/common/http";
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './shared/services/product.service';
 import { RouterModule } from '@angular/router';
+import { SearchFormModule } from './shared/components';
 
 @NgModule({
    declarations: [
@@ -23,7 +24,8 @@ import { RouterModule } from '@angular/router';
       MatIconModule,
       MatToolbarModule,
       FlexLayoutModule,
-      HttpClientModule
+      HttpClientModule,
+      SearchFormModule
    ],
    providers: [ProductService],
    bootstrap: [AppComponent]
